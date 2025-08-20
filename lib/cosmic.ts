8b853964612252b7e0e27bfc910f5984be225c7c
Cosmic AI <support@cosmicjs.com>
2025-08-20 13:54:27 -0700

    edit lib/cosmic.ts

--- lib/cosmic.ts
+++ lib/cosmic.ts
@@ -5,6 +5,7 @@ const cosmic = createBucketClient({
   bucketSlug: process.env.COSMIC_BUCKET_SLUG as string,
   readKey: process.env.COSMIC_READ_KEY as string,
   writeKey: process.env.COSMIC_WRITE_KEY as string,
+  apiEnvironment: "staging"
 })
 
 // Get home page data
